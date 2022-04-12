d9543819a6700c52953b6302a16def27c13468d1
shagors <shajjadshagor@gmail.com>
2022-04-12 20:58:56 +0600

    set header and responsive menu

--- src/Pages/Home/Experts/Experts.js
+++ src/Pages/Home/Experts/Experts.js
@@ -11,9 +11,9 @@ const experts = [
     {id: 1, name: 'B Babu', img: expert1},
     {id: 2, name: 'Silvi Dabbu', img: expert2},
     {id: 3, name: 'Eid Tattu Ghora', img: expert3},
-    {id: 4, name: 'Sarmin Hippo', img: expert4},
+    {id: 4, name: 'A Babu', img: expert4},
     {id: 5, name: 'Mrittika Miti Miti', img: expert5},
-    {id: 6, name: 'A Babu', img: expert6}
+    {id: 6, name: 'Sarmin Hippo', img: expert6}
 ]
 
 const Experts = () => {
