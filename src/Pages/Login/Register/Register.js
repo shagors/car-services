d65e69785a47afb98b825ca91d124f63e96d7a09
shagors <shajjadshagor@gmail.com>
2022-04-13 20:29:05 +0600

    restricted page set up and without login page locked and set private route

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import {
+    useCreateUserWithEmailAndPassword
+} from 'react-firebase-hooks/auth';
 import './Register.css'
+import auth from '../../../firebase.init';
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth);
     const navigate= useNavigate();
 
     const navigateLogin = () => {
         navigate('/login');
+    }
+    if(user){
+        navigate('/home');
     }
 
     const handleRegister = event => {
@@ -15,7 +28,7 @@ const Register = () => {
         const email = event.target.email.value;
         const password = event.target.password.value;
 
-        
+        createUserWithEmailAndPassword(email, password);
     }
 
 
