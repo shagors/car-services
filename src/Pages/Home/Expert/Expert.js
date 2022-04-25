8c390628e1292e62a55267e5edb305afd31cdc73
shagors <shajjadshagor@gmail.com>
2022-04-25 15:27:09 +0600

    clent side add and connect with serve and jwt tokens and email validation check

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -5,7 +5,7 @@ const Expert = ({expert}) => {
     return (
         <div className='g-4 col-sm-12 col-md-6 col-lg-4'>
             <div className='card'>
-                <img src={img} class="card-img-top" alt="..."/>
+                <img src={img} className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">{name}</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
